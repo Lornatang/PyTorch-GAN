9f6c321e41f0603c2f7b123c74bcc2bb5af91fd5
lorna <shiyipaisizuo@163.com>
2019-08-20 14:59:38 +0800

    fix some error

--- source/dcgan/dcgan.py
+++ source/dcgan/dcgan.py
@@ -38,7 +38,7 @@ from torchvision import utils as vutils
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataroot', type=str, default='~/pytorch_datasets', help='path to dataset')
-parser.add_argument('--workers', type=int, help='number of data loading workers', default=8)
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument('--batch_size', type=int, default=256, help='inputs batch size')
 parser.add_argument('--image_size', type=int, default=32, help='the height / width of the inputs image to network')
 parser.add_argument('--channels', type=int, default=3, help='image rgb or gray image')

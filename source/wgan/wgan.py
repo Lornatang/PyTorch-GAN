89e7180c8d3da8395fe9c75f6ae2d45db644ecd3
Changyu_Liu <shiyipaisizuo@163.com>
2019-08-23 09:40:36 +0800

    Update wgan.py

--- source/wgan/wgan.py
+++ source/wgan/wgan.py
@@ -159,7 +159,7 @@ class Discriminator(nn.Module):
       forwarded data.
     """
     outputs = self.main(inputs)
-    return outputs.view(-1, 1024 * 4 * 4)
+    return outputs.view(-1, 1).squeeze(1)
 
 
 fixed_noise = torch.randn(opt.batch_size, nz, 1, 1, device=device)

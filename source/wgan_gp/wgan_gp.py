3228836409750956f7968771caf2dcacf125081f
Changyu_Liu <shiyipaisizuo@163.com>
2019-08-23 11:47:47 +0800

    Update wgan_gp.py

--- source/wgan_gp/wgan_gp.py
+++ source/wgan_gp/wgan_gp.py
@@ -167,11 +167,11 @@ class Discriminator(nn.Module):
 def compute_gradient_penalty(D, real_samples, fake_samples):
   """Calculates the gradient penalty loss for WGAN GP"""
   # Random weight term for interpolation between real and fake samples
-  alpha = torch.tensor(np.random.random((real_samples.size(0), 1, 1, 1)))
+  alpha = torch.tensor(np.random.random((real_samples.size(0), 1, 1, 1))).to(device)
   # Get random interpolation between real and fake samples
-  interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_()
+  interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True)
   d_interpolates = D(interpolates)
-  fake = Variable(torch.tensor(real_samples.shape[0], 1).fill_(1.0), requires_grad=False)
+  fake = Variable(torch.tensor(real_samples.shape[0], 1).fill_(1.0), requires_grad=False).to(device)
   # Get gradient w.r.t. interpolates
   gradients = autograd.grad(
     outputs=d_interpolates,
